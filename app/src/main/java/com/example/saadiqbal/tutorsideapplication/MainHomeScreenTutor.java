d3a104630c0a691cf6d03542865de3a5bbefcaff
msi027 <saad@ahmedgraf.com>
2018-02-25 14:57:10 +0500

    Initial commit.

---
+++ app/src/main/java/com/example/saadiqbal/tutorsideapplication/MainHomeScreenTutor.java
@@ -0,0 +1,13 @@
+package com.example.saadiqbal.tutorsideapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainHomeScreenTutor extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_home_screen_tutor);
+    }
+}
