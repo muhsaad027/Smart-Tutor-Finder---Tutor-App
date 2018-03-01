a44026c25f104403c936dc9c539c4ac1d8c24ccf
msi027 <saad@ahmedgraf.com>
2018-03-01 14:33:44 +0500

    Initial commit.

---
+++ app/src/main/java/com/example/saadiqbal/tutorsideapplication/DayTimeBooking.java
@@ -0,0 +1,13 @@
+package com.example.saadiqbal.tutorsideapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DayTimeBooking extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_day_time_booking);
+    }
+}
