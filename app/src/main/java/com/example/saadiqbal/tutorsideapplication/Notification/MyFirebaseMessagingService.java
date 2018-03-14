423b49673abf032224bc0a8cbb08f48b6667b33a
msi027 <saad@ahmedgraf.com>
2018-03-14 21:01:17 +0500

    Initial commit.

--- app/src/main/java/com/example/saadiqbal/tutorsideapplication/Notification/MyFirebaseMessagingService.java
+++ app/src/main/java/com/example/saadiqbal/tutorsideapplication/Notification/MyFirebaseMessagingService.java
@@ -8,6 +8,7 @@ import android.provider.Settings;
 import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
+import com.example.saadiqbal.tutorsideapplication.MainHomeScreenTutor;
 import com.example.saadiqbal.tutorsideapplication.MainScreen;
 import com.example.saadiqbal.tutorsideapplication.R;
 import com.google.firebase.messaging.FirebaseMessagingService;
@@ -56,21 +57,47 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
                         .setSmallIcon(R.mipmap.ic_launcher)
                         .setContentTitle("" + notificationObject.getString("title"))
                         .setContentText("" + notificationObject.getString("message"));
-        Intent notificationIntent = new Intent(this, MainScreen.class);
-        notificationIntent.putExtra("title", notificationObject.getString("title"));
-        notificationIntent.putExtra("phoneNo", notificationObject.getString("phoneNo"));
-        notificationIntent.putExtra("type", notificationObject.getString("type"));
-        notificationIntent.putExtra("reqId", notificationObject.getString("reqId"));
-        notificationIntent.putExtra("latitude", notificationObject.getString("latitude"));
-        notificationIntent.putExtra("longitude", notificationObject.getString("longitude"));
+
+        Log.d("Form 1",notificationObject.getString("type"));
+
+        switch (notificationObject.getString("type")){
+            case "Tution ":
+
+                Intent notificationIntent = new Intent(this, MainScreen.class);
+                notificationIntent.putExtra("title", notificationObject.getString("title"));
+                notificationIntent.putExtra("phoneNo", notificationObject.getString("phoneNo"));
+                notificationIntent.putExtra("type", notificationObject.getString("type"));
+                notificationIntent.putExtra("reqId", notificationObject.getString("reqId"));
+                notificationIntent.putExtra("latitude", notificationObject.getString("latitude"));
+                notificationIntent.putExtra("longitude", notificationObject.getString("longitude"));
 
 
-        PendingIntent contentIntent = PendingIntent.getActivity(this, 0, notificationIntent,
-                PendingIntent.FLAG_UPDATE_CURRENT);
-        builder.setContentIntent(contentIntent);
-        builder.setSound(Settings.System.DEFAULT_NOTIFICATION_URI);
+                PendingIntent contentIntent = PendingIntent.getActivity(this, 0, notificationIntent,
+                        PendingIntent.FLAG_UPDATE_CURRENT);
+                builder.setContentIntent(contentIntent);
+                builder.setSound(Settings.System.DEFAULT_NOTIFICATION_URI);
 
-        NotificationManager manager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-        manager.notify(0, builder.build());
+                NotificationManager manager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+                manager.notify(0, builder.build());
+
+                break;
+            case "FinishTution":
+                Intent notificationIntents = new Intent(this, MainHomeScreenTutor.class);
+                notificationIntents.putExtra("title", notificationObject.getString("title"));
+                notificationIntents.putExtra("type", notificationObject.getString("type"));
+                notificationIntents.putExtra("reqId", notificationObject.getString("reqId"));
+                notificationIntents.putExtra("message", notificationObject.getString("message"));
+
+
+                PendingIntent contentIntents = PendingIntent.getActivity(this, 0, notificationIntents,
+                        PendingIntent.FLAG_UPDATE_CURRENT);
+                builder.setContentIntent(contentIntents);
+                builder.setSound(Settings.System.DEFAULT_NOTIFICATION_URI);
+
+                NotificationManager managers = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+                managers.notify(0, builder.build());
+                break;
+
+        }
     }
 }
